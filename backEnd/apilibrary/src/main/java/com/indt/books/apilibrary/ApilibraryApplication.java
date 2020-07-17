a934f5fbaf2d2ad0c62dbadad4a1fcb68ad12579
rmenna <rmbb.hef2.hef@uea.edu.br>
2020-07-17 08:36:52 -0400

    Add Package Models

---
+++ backEnd/apilibrary/src/main/java/com/indt/books/apilibrary/ApilibraryApplication.java
@@ -0,0 +1,13 @@
+package com.indt.books.apilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilibraryApplication.class, args);
+	}
+
+}
---
+++ backEnd/apilibrary/src/main/java/com/indt/books/apilibrary/models/Book.java
@@ -0,0 +1,71 @@
+package com.indt.books.apilibrary.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_BOOK")
+public class Book implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private String autor;
+	
+	private int isbn;
+	
+	private boolean isValid = true;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	
+	public boolean isValid() {
+		return isValid;
+	}
+
+	public void setValid(boolean isValid) {
+		this.isValid = isValid;
+	}
+}
